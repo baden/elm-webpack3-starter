b8a910f7048cbb1432d5bf93095cd7e48e39b0da
Denys Batrak <baden.i.ua@gmail.com>
2017-10-03 20:58:54 +0300

    Dirty example for multiple IncDec components on a page.

--- src/App/Pages/Home.elm
+++ src/App/Pages/Home.elm
@@ -19,30 +19,50 @@ import Return.Optics exposing (refractl)
 
 type alias Model =
     { counter : Time
-    , incdec : IncDec.Model
+    , incdec1 : IncDec.Model
+    , incdec2 : IncDec.Model
     }
 
 
-incdecl : Lens Model IncDec.Model
-incdecl =
-    Lens .incdec <| \u m -> { m | incdec = u }
+type IncDecs
+    = First
+    | Second
+
+
+incdec1l : Lens Model IncDec.Model
+incdec1l =
+    Lens .incdec1 <| \u m -> { m | incdec1 = u }
+
+
+incdec2l : Lens Model IncDec.Model
+incdec2l =
+    Lens .incdec2 <| \u m -> { m | incdec2 = u }
 
 
 type Msg
     = Tick Time
-    | IncDecMessage IncDec.Msg
+    | IncDecMessage IncDecs IncDec.Msg
     | NoOp
 
 
 init : ( Model, Cmd Msg )
 init =
     let
-        ( incdecModel, incdecCmd ) =
+        ( incdec1Model, incdec1Cmd ) =
             IncDec.init
-                |> Return.mapCmd IncDecMessage
+                |> Return.mapCmd (IncDecMessage First)
+
+        ( incdec2Model, incdec2Cmd ) =
+            IncDec.init
+                |> Return.mapCmd (IncDecMessage Second)
     in
-        Return.singleton { counter = 0, incdec = incdecModel }
-            |> Return.command incdecCmd
+        Return.singleton
+            { counter = 0
+            , incdec1 = incdec1Model
+            , incdec2 = incdec2Model
+            }
+            |> Return.command incdec1Cmd
+            |> Return.command incdec2Cmd
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -55,8 +75,17 @@ update msg =
             Tick newTime ->
                 Return.map <| \model -> { model | counter = newTime }
 
-            IncDecMessage subMsg ->
-                refractl incdecl IncDecMessage <| IncDec.update subMsg
+            IncDecMessage index subMsg ->
+                let
+                    incdecl =
+                        case index of
+                            First ->
+                                incdec1l
+
+                            Second ->
+                                incdec2l
+                in
+                    refractl incdecl (IncDecMessage index) <| IncDec.update subMsg
 
 
 view : Model -> Html Msg
@@ -65,11 +94,11 @@ view model =
         [ text <| "HOME: WIP" ++ toString model
         , div [ class "row" ]
             [ div [ class "col-sm-6" ]
-                [ IncDec.view model.incdec |> Html.map IncDecMessage
+                [ IncDec.view model.incdec1 |> Html.map (IncDecMessage First)
                 ]
             , div
                 [ class "col-sm-6" ]
-                [ IncDec.view model.incdec |> Html.map IncDecMessage
+                [ IncDec.view model.incdec2 |> Html.map (IncDecMessage Second)
                 ]
             ]
         ]
@@ -79,6 +108,6 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ Time.every second Tick
-        , IncDec.subscriptions model.incdec
-            |> Sub.map IncDecMessage
+        , IncDec.subscriptions model.incdec1 |> Sub.map (IncDecMessage First)
+        , IncDec.subscriptions model.incdec2 |> Sub.map (IncDecMessage Second)
         ]
